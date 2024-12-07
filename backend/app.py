5d6c2e4b91c150228478fde8a2585ab78e2f2fe1
desa-thomas <desa2thomas@gmail.com>
2024-12-06 22:15:06 -0500

    updated upload_image()

--- backend/app.py
+++ backend/app.py
@@ -1,27 +1,41 @@
-from flask import Flask, request, jsonify
+import base64
+from flask import Flask, request, jsonify, render_template
 import cv2
+import numpy as np
+import io
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<p>root/</p>' 
+    return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
+@app.route('/classify', methods=['POST'])
 def upload_image():
-    #Check if image file was uploaded
-    if 'image' not in request.files:
-        return jsonify({'error': 'No image uploaded'}), 400
 
-    #get image from request
-    image_file = request.files['image']
-
-    try: 
-        image = cv2.imread(image_file)
-
-    except Exception as e: 
-        return jsonify({'error': str(e)}), 500
+    data = request.get_json()
+    img_data = data.get('image')
     
+    #if image present
+    if img_data: 
+        #remove 'data:image/png;base64' prefix
+        img_data = img_data.split(',')[1]
+        img_bytes = base64.b64decode(img_data)
+        with open("debug_image.png", "wb") as f:
+            f.write(img_bytes)
+
+        img_arr = np.frombuffer(img_bytes, dtype=np.uint8)
+        image = cv2.imdecode(img_arr, cv2.IMREAD_GRAYSCALE)
+        
+        #Debug, check that the image uploaded
+        # if image is not None:
+        #     cv2.imshow('Image', image)
+        #     cv2.waitKey(0)
+        #     cv2.destroyAllWindows()
+
+        return jsonify({'message': "Image uploaded successfully"}), 200
+    else:
+        return jsonify({'error': "No image data found"}), 400
 
 if __name__ =='__main__':
     app.run(debug=True)
