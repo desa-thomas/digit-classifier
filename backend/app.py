786ce23cd76ef093516b1bc19cdf31fab01f518a
desa-thomas <desa2thomas@gmail.com>
2024-12-07 01:54:19 -0500

    used waitress to serve website to local devcies

--- backend/app.py
+++ backend/app.py
@@ -3,21 +3,25 @@ from flask import Flask, request, jsonify, render_template
 import cv2
 import numpy as np
 import model
+from waitress import serve
 
 app = Flask(__name__)
 
+#page index
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/classify', methods=['POST'])
 def upload_image():
-
+    """
+    API endpoint for getting model predictions from uploaded image
+    """
     data = request.get_json()
     img_data = data.get('image')
 
     #if image present
-    if img_data: 
+    if img_data:
         #remove 'data:image/png;base64' prefix
         img_data = img_data.split(',')[1]
         img_bytes = base64.b64decode(img_data)
@@ -25,7 +29,7 @@ def upload_image():
         img_arr = np.frombuffer(img_bytes, dtype=np.uint8)
         image = cv2.imdecode(img_arr, cv2.IMREAD_GRAYSCALE)
 
-
+        #get prediction from model, then server prediction
         prediction = model.predict(image)
         
         return jsonify({'message': "Image uploaded successfully", "prediction": prediction}), 200
@@ -33,5 +37,8 @@ def upload_image():
         return jsonify({'error': "No image data found"}), 400
 
 if __name__ =='__main__':
-    app.run(debug=True)
+    print('running on: http://localhost:8080/')
+    serve(app, host = "0.0.0.0", port=8080)
 
+    #Development server
+    #app.run(debug=True)
